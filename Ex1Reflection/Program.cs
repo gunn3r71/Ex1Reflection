b2526f71abda7836b52d2f5286b84a0893897139
Lucas <lucasgunner3@gmail.com>
2021-11-30 22:27:10 -0300

    Finalizando exercicio

--- Ex1Reflection/Program.cs
+++ Ex1Reflection/Program.cs
@@ -1,12 +1,82 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace Ex1Reflection
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.Write("Insira o nome de uma classe: ");
+                var input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input)) break;
+
+                var type = Type.GetType(input);
+
+                var methods = type?.GetMethods();
+
+                if (methods == null) continue;
+
+                foreach (var method in methods)
+                {
+                    if (!method.IsPublic 
+                        || method.IsStatic 
+                        || method.GetParameters().Length > 0 
+                        && method.GetParameters().All(x => x.ParameterType.GetTypeInfo() != typeof(string))) 
+                        continue;
+
+                    Console.WriteLine($"method: {method.Name}");
+                }
+
+                Console.Write("Insira o nome do método para executar: ");
+                input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input)) break;
+
+                var userInputedMethod = type.GetMethod(input);
+
+                if (userInputedMethod is null) continue;
+
+                var t = Activator.CreateInstance(type, null);
+
+                if (userInputedMethod.GetParameters().Any())
+                {
+                    object[] parameters = new object[userInputedMethod.GetParameters().Length];
+                    var parameterInfos = userInputedMethod.GetParameters();
+
+                    for (var i = 0; i < parameterInfos.Length; i++)
+                    {
+                        Console.Write($"Insira o valor para {parameterInfos[i].Name}: ");
+                        var value = Console.ReadLine();
+                        parameters[i] = value;
+                    }
+
+                    userInputedMethod.Invoke(t, parameters);
+                }
+                else
+                {
+                    userInputedMethod.Invoke(t, null);
+                }
+            }
         }
+    }
+
+    public class Teste
+    {
+        public void SayHelloTo(string name) => Console.WriteLine($"Hello {name}!");
+
+        public void CountToTen()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static void SayHello() => Console.WriteLine($"Hello guest!");
     }
 }
